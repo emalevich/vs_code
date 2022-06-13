a67a3ace01137bbd2effe269aa071b38ccbd2350
elenamalevich <106012963+emalevich@users.noreply.github.com>
2022-06-13 21:59:09 +0300

    Add files via upload

---
+++ 1_30.js
@@ -0,0 +1,36 @@
+let user_name = "Helen"
+// let name2 = "Helen"
+
+//console.log(name2)
+
+let age = 27
+// console.log(age)
+
+let person = user_name + " " + age
+// console.log (person)
+
+let user_age = 30
+let work_period = 10
+
+let total_time = user_age * work_period
+// console.log("total_time = ", total_time)
+
+let morning = false
+console.log("morning = ", morning, typeof(morning))
+
+let evening = true
+console.log("evening = ", evening, typeof(evening))
+
+let apples = 15
+let cherry = 20
+let compare = apples >= cherry
+console.log("compare is", compare)
+
+if (compare){
+    console.log("compare is", compare)
+    } else if (apples == 15) {
+        console.log("apples ==", apples)
+    }
+    else {
+        console.log("!!Compare is", false)
+    }
