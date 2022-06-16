dea2d33aafeb3ca6b19801af95a438cd20272886
Alena Malevich <alenamalenalavnik@gmail.com>
2022-06-16 20:37:52 +0300

    amendment

--- hw_1_JS.js
+++ hw_1_JS.js
@@ -34,7 +34,7 @@ item_6 = 15
 //20.Присвоить переменной item_6_type тип переменной item_6
 item_6_type = typeof(item_6) 
 //21.Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——
-console.log("item_6 == " + typeof(item_6), "item_6_type == " + item_6_type)
+console.log("item_6 == " + item_6, "item_6_type == " + item_6_type)
 //22.Создать переменную item_7 и в ней преобразовать item_6 в String.
 let item_7 = String(item_6)
 //23.Создать переменную item_7_type 
@@ -42,7 +42,7 @@ let item_7_type
 //24.Присвоить переменной item_7_type тип переменной item_7
 item_7_type = typeof(item_7)
 //25.Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——
-console.log("item_7 == " + typeof(item_7), "item_7_type == " + item_7_type)
+console.log("item_7 == " + item_7, "item_7_type == " + item_7_type)
 //26.Создать переменную “age_1” и присвоить ей значение 10
 let age_1 = 10
 //27.Создать переменную “age_2” и присвоить ей значение 18
@@ -51,6 +51,7 @@ let age_2 = 18
 let age_3 = 60
 //29.Создать if в котором будите проверять значение переменной age_1
 //30.Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “ It’s less then ”//
+//31-32-33
 if (age_1 < age_2){console.log("You don’t have access cause your age is " + age_1 + "." + " It’s less then " )}
 else if (age_1 >=  age_2, age_1 < age_3){console.log("Welcome")}
 else if (age_1  > age_3){console.log("Keep calm and look Culture channel")}
