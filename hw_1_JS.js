1c98779f5a05f6f1ff2623b90224a0997d5a3a8a
elenamalevich <106012963+emalevich@users.noreply.github.com>
2022-06-17 09:14:16 +0300

    Update hw_1_JS.js

--- hw_1_JS.js
+++ hw_1_JS.js
@@ -53,6 +53,6 @@ let age_3 = 60
 //30.Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “ It’s less then ”//
 //31-32-33
 if (age_1 < age_2){console.log("You don’t have access cause your age is " + age_1 + "." + " It’s less then " )}
-else if (age_1 >=  age_2, age_1 < age_3){console.log("Welcome")}
+else if (age_1 >=  age_2 && age_1 < age_3){console.log("Welcome")}
 else if (age_1  > age_3){console.log("Keep calm and look Culture channel")}
 else {console.log("Technical work")}
